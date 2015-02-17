684dba123a183dc5c3d7aad522145989fd674c04
kotarCreative <kotarcreative@gmail.com>
2015-02-17 15:15:40 -0700

    Add header for all webpages. Fix routing

--- app/routes.php
+++ app/routes.php
@@ -4,14 +4,15 @@ Route::get('/', 'PagesController@home');
 
 
 Route::get('/signIn', 'PagesController@signIn');
-
 Route::get('/calendar', ['as' => 'home', function(){
     return View::make('calendar');
 }]);
-
-
 Route::resource('sessions', 'SessionsController');
 Route::resource('users', 'UsersController');
-
 Route::resource('hardware', 'HardwareController');
 Route::resource('hardwaretype', 'HardwareTypeController');
+Route::resource('users', 'UsersController');
+Route::resource('hardware', 'HardwareController');
+Route::get('/reportdamage', ['as' => 'reportdamage', function(){return View::make('reportdamage');}]);
+Route::get('/browsekits', ['as' => 'browsekits', function(){ return View::make('browsekits');}]);
+Route::get('/home', ['as' => 'home',function(){ return View::make('home');}]);
