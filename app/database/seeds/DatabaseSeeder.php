aab0b5e11b953c67d0a3fe12351bb58858c40775
Derrick <derrick.b.gold@gmail.com>
2015-03-09 13:28:26 -0600

    Add hardware controller Add hardwaretype controller Add seed for hardware type Add missing code to UserController

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 		$this->call('UserTableSeeder');
+		$this->call('HardwareTypeTableSeeder');
 	}
 
 }
@@ -21,7 +22,7 @@ class UserTableSeeder extends Seeder {
     public function run()
     {
 		
-		DB::TABLE('users')->delete();
+		DB::table('users')->delete();
         User::create(array('email' => 'foo@bar.com', 
 						  'branchID' => '1',
 						  'password' => 'test',
@@ -31,4 +32,24 @@ class UserTableSeeder extends Seeder {
 						  ));
     }
 
+}
+
+
+class HardwareTypeTableSeeder extends Seeder {
+	
+	public function run() 
+	{
+		DB::table('hardwareType')->delete();
+		
+		HardwareType::create(array(
+			"name" => "iPad",
+			"description" => "A touch screen device that does magic!"
+		));
+		
+		HardwareType::create(array(
+			"name" => "Xbox 360",
+			"description" => "360 degrees of nothingness."
+		));
+
+	}
 }
