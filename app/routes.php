c35f47ace0039a9bd3581d22396a4246d222d38a
jharvard <kotarcreative@gmail.com>
2015-03-09 15:37:36 -0400

    Create webpage header

--- app/routes.php
+++ app/routes.php
@@ -12,3 +12,4 @@ Route::resource('hardwaretype', 'HardwareTypeController');
 Route::get('/reportdamage', 'PagesController@showReportDamage');
 Route::get('/browsekits', ['as' => 'browsekits', function(){ return View::make('browsekits');}]);
 Route::get('/', ['as' => 'signin',function(){ return View::make('signIn');}]);
+
