6e444465bdc82b0533c39c4bf8e2b44c1d3cd25e
jharvard <kotarcreative@gmail.com>
2015-03-09 22:29:42 -0400

    Finalize header, Add routing to blank pages for all buttons.

--- app/routes.php
+++ app/routes.php
@@ -6,9 +6,6 @@ Route::get('/overview', 'PagesController@overview');
 
 
 Route::get('/signIn', 'PagesController@signIn');
-Route::get('/calendar', ['as' => 'home', function(){
-    return View::make('calendar');
-}]);
 Route::resource('sessions', 'SessionsController');
 Route::resource('users', 'UsersController');
 Route::resource('hardware', 'HardwareController');
--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,7 +1,2 @@
-@extends('layouts.admin')
 
-@section('items')
-        <li>
-            <div id="link">{{ link_to("/signIn", "Login") }}</div>
-        </li>
-@stop
+@extends('layouts.admin')
