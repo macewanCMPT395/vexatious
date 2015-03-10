ee556991cc232e8b5bcaefb070a7da1aaaf61963
jharvard <kotarcreative@gmail.com>
2015-03-09 22:29:42 -0400

    Finalize header, Add routing to blank pages for all buttons.

--- app/routes.php
+++ app/routes.php
@@ -2,8 +2,8 @@
 
 Route::get('/', 'PagesController@home');
 Route::get('/signIn', 'PagesController@signIn');
-Route::post('/validate', array('as' => 'users.validate', 'uses' => 'UsersController@validate'));
 Route::resource('users', 'UsersController');
 Route::resource('hardware', 'HardwareController');
 Route::get('/reportdamage', ['as' => 'reportdamage', function(){return View::make('reportdamage');}]);
 Route::get('/browsekits', ['as' => 'browsekits', function(){ return View::make('browsekits');}]);
+Route::get('/home', ['as' => 'home',function(){ return View::make('home');}]);
