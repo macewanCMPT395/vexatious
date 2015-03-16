cdbf5fc83e51bd58caa985a7616d7ff402d669ee
jharvard <kotarcreative@gmail.com>
2015-03-16 14:27:19 -0400

    Fix routing to controllers instead of explicit addresses, Add kits directory in views with relevant files

--- app/routes.php
+++ app/routes.php
@@ -2,10 +2,11 @@
 
 Route::get('/home', 'PagesController@home');
 Route::get('/signIn', 'PagesController@signIn');
+Route::get('/', 'PagesController@signIn');
+Route::get('/test',['as' => 'home',function() { return View::make('home');}]);
 Route::resource('sessions', 'SessionsController');
 Route::resource('users', 'UsersController');
 Route::resource('hardware', 'HardwareController');
 Route::resource('hardwaretype', 'HardwareTypeController');
 Route::get('/reportdamage', 'PagesController@showReportDamage');
-Route::get('/browsekits', ['as' => 'browsekits', function(){ return View::make('browsekits');}]);
-Route::get('/', ['as' => 'signin',function(){ return View::make('signIn');}]);
+Route::resource('kits','KitController');
