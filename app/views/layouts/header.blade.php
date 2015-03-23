eb1bcfebcaf5d77e5d6fa7ffdc2bcf6e2957409c
KotarCreative <kotarcreative@gmail.com>
2015-03-23 13:27:03 -0400

    Remove conflict messages

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -6,7 +6,6 @@
 </head>
     
 <body>
-<<<<<<< HEAD
 <div id="Image"><img src="http://www.epl.ca/sites/all/themes/epl/img/EPL_logo2.png" border="0" style="width:400px;"></div>
 <div class="navMenu">
 <ul>
@@ -20,16 +19,6 @@
     {{ HTML::linkRoute('browsekits', 'Browse Kits') }} </li>
 @yield('adminItems')
 </ul>
-=======
-<div class="menu">  
-<div id="Image"><img src="http://www.epl.ca/sites/all/themes/epl/img/epl_logo.jpg" border="0"></div>
-<div id="bar"></div>
-<div class="button">{{ HTML::linkRoute('home','Overview') }}</div>
-<div class="button">{{ HTML::linkRoute('kits.show', 'Book Kit') }}</div>
-<div class="button">{{ HTML::linkRoute('home', 'Shipping') }}</div>
-<div class="button">{{ HTML::linkRoute('kits.index', 'Browse Kits') }}</div>
-@yield('adminButtons')
->>>>>>> Add route to username and fix logo position.
 </div>
 @yield('content')
 </body>
