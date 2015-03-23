75cc478412ca886b2fcc15f52ad2004ce28faafe
KaroAntonio <karoantonio@gmail.com>
2015-03-23 11:07:07 -0600

    Page Content

---
+++ app/views/bookkit.blade.php
@@ -0,0 +1,133 @@
+@extends('layouts.admin')
+
+@section('headerScript')
+{{ HTML::style('css/calendar.css') }}
+{{ HTML::style('css/bookkit.css') }}
+<link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
+<script src='fullcalendar/lib/jquery.min.js'></script>
+<script src='fullcalendar/lib/moment.min.js'></script>
+<script src='fullcalendar/fullcalendar.js'></script>
+<script type='text/javascript' src='fullcalendar/gcal.js'></script>
+<script> 
+
+$('td.fc-day-number').mouseover(function ()
+{
+var strDate = $(this).data('date');
+$("td.fc-day").filter("[data-date='" + strDate + "']").addClass('fc-highlight')
+});
+    
+//LOAD Bookings into events array
+var bookings = <?php echo json_encode($bookings); ?>;
+var eventsArray = [];
+for (var i = 0; i < bookings.length; i++) {
+    eventsArray.unshift({
+        title: bookings[i].eventName,
+        start: parseDate(bookings[i].start),
+        end: parseDate(bookings[i].end),
+        tip: 'Destination: ' + bookings[i].destination,
+        color: '#7CC045',
+        allDay: true    
+        });
+}
+
+var selectedDay;
+$(document).ready(function() {
+    $('#calendar').fullCalendar({
+        header: {
+            left: ' title',
+            center: '',
+            right: 'prev,next today'
+        },
+        buttonIcons: {
+            //prev: 'left-single-arrow',
+            //next: 'right-single-arrow'
+        },
+        //theme: true,
+        contentHeight: 600,
+        //defaultDate: '2015-04-12',
+        editable: false,
+        eventLimit: true, // allow "more" link when too many events
+        events: eventsArray,
+        eventRender: function(event, element) {
+            element.attr('title', event.tip);
+        },
+        dayClick: function(date, jsEvent, view) {
+            //CHECK if day can be selected
+            //Must not be in the past, 
+            //Must have kits available
+            selectDay($(this));
+            
+            //Auto-Fill Date Fields
+            var start = document.getElementById('startField'),
+                end = document.getElementById('endField');
+            start.value = end.value = date.format();
+        }
+    });
+});
+    
+function selectDay(day){
+    if (selectedDay != null)
+        selectedDay.css('background-color', 'white');
+    selectedDay = day;
+    selectedDay.css('background-color', '#FBBC2E');
+}
+    
+function parseDate(date) {
+    var day = date.substring(0,2);
+    //Decrement Month to convert to month system starting with 0
+    var month = date.substring(3,5) - 1; 
+    var year = date.substring(6,10);
+    
+    return new Date(year, month, day);
+}
+</script>
+@stop
+@section('bookkitli') class="active" @stop
+@section('content')
+<div class="content">
+<div class="bookKitForm">
+    {{ Form::open(['method' => 'get', 'route' => 'bookkit']) }}
+    <div class="title"></div>
+    <ul class="formFields">
+    <li>
+    {{ Form::label('type', 'Type') }}
+    {{ Form::select('type', array('ipad' => 'iPad', 'zune' => 'Zune')) }}
+    </li>
+    <li>
+    {{ Form::label('eventName', 'Event Name') }}
+    {{ Form::text('eventName') }}
+    </li>
+    <li>
+    {{ Form::label('start', 'Start') }}
+    {{ Form::input('date', 'start',  null, ['id' => 'startField']) }}
+    </li>
+    <li>
+    {{ Form::label('end', 'End') }}
+    {{ Form::input('date', 'end',  null, ['id' => 'endField']) }}
+    </li>
+    <li>
+    {{ Form::label('branch', 'Branch') }}
+    {{ Form::select('type', array('dt' => 'Downtown', 'ut' => 'Uptown')) }}
+    </li>
+    <li>
+    {{ Form::label('barcode', 'Barcode ') }}
+    {{ Form::text('barcode') }}
+    </li>
+    <li>
+    {{ Form::submit('Book Kit') }}
+    </li>
+    </ul>
+    {{ Form::close() }}
+</div>
+<div class="tabs">
+    <ul class="tabs">
+        <li>
+          <input type="radio" checked name="tabs" id="tab1">
+          <div id="tab-content1" class="tab-content animated fadeIn">
+              <div id="calendar"></div>
+          </div>
+        </li>
+</ul>
+</div>
+</div>
+@stop
---
+++ app/views/shipping.blade.php
@@ -0,0 +1,95 @@
+@extends('layouts.admin')
+@section('headerScript')
+{{ HTML::style('css/kits.css') }}
+{{ HTML::style('css/shipping.css') }}
+<script src='fullcalendar/lib/jquery.min.js'></script>
+<script src='fullcalendar/lib/moment.min.js'></script>
+@stop
+@section('shippingli') class="active" @stop
+@section('content')
+
+
+<div id="todayTable">
+<div class="tableTitle">Today </div>
+<div id="todayDate"> [Today's Date] </div>
+<table class="kitsTable">
+<thead>
+<tr>
+    <th>Description</th>
+    <th>Barcode</th>
+    <th>Destination Branch ID</th>
+</tr>
+</thead>
+</table>
+<div class="tableRows">
+<table class="kitsTable kitRows">
+<tbody>
+    @for ($i = 0; $i < 100; $i++) 
+        @foreach ($kits as $kit)
+        <tr>
+            <td>{{$kit->description}}</td>
+            <td>{{$kit->barcode}}</td>
+            <td>{{$kit->currentBranchID}}</td>
+        </tr>
+        @endforeach
+    @endfor
+</tbody>
+</table>
+</div>
+</div>
+
+
+
+<div id="tomorrowTable">
+<div class="tableTitle">Tomorrow </div>
+<div id="tomorrowDate"> [Tomorrow's Date] </div>
+<table class="kitsTable">
+<thead>
+<tr>
+    <th>Description</th>
+    <th>Barcode</th>
+    <th>Destination Branch ID</th>
+</tr>
+</thead>
+</table>
+<div class="tableRows">
+<table class="kitsTable kitRows">
+<tbody>
+    @for ($i = 0; $i < 100; $i++) 
+        @foreach ($kits as $kit)
+        <tr>
+            <td>{{$kit->description}}</td>
+            <td>{{$kit->barcode}}</td>
+            <td>{{$kit->currentBranchID}}</td>
+        </tr>
+        @endforeach
+    @endfor
+</tbody>
+</table>
+</div>
+</div>
+
+<script>
+//Set Table Dates
+var today = document.querySelector("#todayDate");
+var tomorrow = document.querySelector("#tomorrowDate");
+
+console.log(moment()[0]);
+    
+today.innerHTML = moment().format("dddd, MMMM Do YYYY");
+tomorrow.innerHTML = moment(today).add(1,'days').format("dddd, MMMM Do YYYY");
+    
+    
+//Make Table Rows selectable
+var rows = document.querySelectorAll(".kitRows tbody tr");
+var selected;
+for(var i = 0; i < rows.length; i++) {
+    rows[i].onclick = function() {
+        if (selected != null)
+            selected.classList.toggle('selected');
+        selected = this;
+        selected.classList.toggle('selected');
+    }
+}
+</script>
+@stop
