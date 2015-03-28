a979f35c7b0f7294b2c1a6fbfa22baa4b3295116
KotarCreative <kotarcreative@gmail.com>
2015-03-28 14:48:44 -0400

    Add edit kit form and CSS

--- app/views/kits/edit.blade.php
+++ app/views/kits/edit.blade.php
@@ -1,1 +1,40 @@
 @extends('layouts.header')
+@section('headerScript')
+{{ HTML::style('css/editkit.css') }}
+<script src='fullcalendar/lib/jquery.min.js'></script>
+@stop
+@section('browsekitsli') class="" @stop
+@section('content')
+<div id="kitInfo">
+     {{ Form::open(['method' => 'post', 'route' => 'kits.edit']) }}
+     	<div class="inputs">
+	<div>
+		{{ Form::label('kitNumber', 'Kit Number:') }}
+    		{{ Form::Input('string','KitNumber') }}
+	</div>
+	<div>
+		{{ Form::label('currentBranch', 'Current Branch:') }}
+		{{ Form::Input('string', 'CurrentBranch') }}
+	</div>
+	<div>
+		{{ Form::label('description', 'Description:') }}
+		{{ Form::Input('string', 'description') }}
+	</div>
+	</div>
+	<div>
+	{{ Form::label('assetTable','Items within this kit') }}
+	<table id="assetTable">
+		<thead>
+			<th>Type</th>
+			<th>Damaged?</th>
+		<thead>
+		<tbody>
+		</tbody>
+	</table>
+	</div>
+	<div>
+	{{ Form::Submit('Apply Changes') }}
+	</div>		       
+{{ Form::close() }}
+</div>
+@stop
