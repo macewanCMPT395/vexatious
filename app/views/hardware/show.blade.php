da4e354fbf6b93ef11a53dbee246fe32d1780e4b
Derrick <derrick.b.gold@gmail.com>
2015-03-28 23:00:26 -0600

    Add back individual hardware view page

---
+++ app/views/hardware/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.admin')
+
+@section('headerScript')
+{{ HTML::script('fullcalendar/lib/jquery.min.js') }}
+{{ HTML::script('js/Hardware.js') }}
+{{ HTML::script('js/post.js') }}
+{{ HTML::style('css/hardware.css') }}
+<script>
+	
+$(document).ready(function() {
+	var kitID = {{ $hardware->id }}
+
+
+	var HWForm = HardwareForm.init({
+		hwInfoRoute: "{{ route('hardware.show'); }}",
+		kitInfoRoute: "{{ route('kits.edit'); }}"
+	});
+	
+	HWForm.getStart(function(){
+		$('.loadingImg').show();
+		console.log("start");
+	});
+	HWForm.getDone(function(){
+		console.log("Done");
+		$('.loadingImg').hide();
+	});
+
+	HWForm.fill(kitID);
+	//HWForm.post();
+});
+</script>
+
+@stop
+
+@section('content')
+	@include('layouts.hardware')
+
+	<div class="loadingImg">
+		<img src="/images/loading_spinner.gif"/>
+	</div>
+@stop
